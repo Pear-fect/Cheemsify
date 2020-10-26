35cd84c43cf48d591035a63ddd85e6a9d297d9f0
Ebbe A <ebbe.arildsson@gmail.com>
2020-10-26 22:17:29 +0100

    update #9

--- cheemsify.js
+++ cheemsify.js
@@ -1,11 +1,22 @@
 javascript:(function(){ 
+    const matchAllExceptWhitespace = /\w+/g
+    document.body.innerHTML.replace(matchAllExceptWhitespace, "test");
     /*
     let pageText = document.body.innerText.split(/[\n\s]/)
     for(let i = 0; i < pageText.length; i++){
         if(pageText[i].length > 2 && /^[a-zA-Z]+$/.test(pageText[i])) console.log(pageText[i] + ": " + i)
     }
     document.body.innerText = pageText
-    */
+
     document.body.innerHTML = document.body.innerHTML.replace(/^[a-zA-Z]+$/, "replacement string");
-    
+
+    const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+    const word = 'Lorem';
+
+    let node;
+
+    while (node = treeWalker.nextNode()) {
+        node.textContent = node.textContent.replace(matchAllExceptWhitespace, word);
+    }
+    */
 }())
