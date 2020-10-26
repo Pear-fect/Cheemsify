588ef3f6f9be993fff05778e7f41949a4f5f8f4f
Ebbe A <ebbe.arildsson@gmail.com>
2020-10-26 20:03:51 +0100

    updated cheems

--- cheemsify.js
+++ cheemsify.js
@@ -1,6 +1,1 @@
-(function() {
-    var elts = document.getElementsByTagName('div');
-    for (var i = 0; i < elts.length; i++) {
-      elts[i].style['background-color'] = '#C0C';
-    }
-  })();
+javascript:(function(){ console.log(document.body.innerText)}())
