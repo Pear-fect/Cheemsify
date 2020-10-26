bdd4d919b2da5c7dd505ae4e0ee7a2a19748aab5
Ebbe A <ebbe.arildsson@gmail.com>
2020-10-26 21:37:20 +0100

    update #9

--- cheemsify.js
+++ cheemsify.js
@@ -1,7 +1,11 @@
 javascript:(function(){ 
+    /*
     let pageText = document.body.innerText.split(/[\n\s]/)
     for(let i = 0; i < pageText.length; i++){
         if(pageText[i].length > 2 && /^[a-zA-Z]+$/.test(pageText[i])) console.log(pageText[i] + ": " + i)
     }
     document.body.innerText = pageText
+    */
+    document.body.innerHTML = document.body.innerHTML.replace(/^[a-zA-Z]+$/, "replacement string");
+    
 }())
