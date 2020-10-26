82c9d8d2aa941ce4775e86a696cacda4a64a87e0
Ebbe A <ebbe.arildsson@gmail.com>
2020-10-27 00:55:39 +0100

    WORKING

--- cheemsify.js
+++ cheemsify.js
@@ -1,8 +1,23 @@
 javascript:(function(){ 
-    var html = document.querySelector('html');
-    var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT);
-    var node;
+    var html = document.querySelector('html')
+    var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT)
+    var node
     while (node = walker.nextNode()) {
-      node.nodeValue = node.nodeValue.replace(/\w+/g, 'cheems')
+        if(node.nodeValue.length > 3 && Math.random() > 0){
+            node.nodeValue = node.nodeValue.replace(/an/g, "am")
+            node.nodeValue = node.nodeValue.replace(/es/g, "ems")
+            node.nodeValue = node.nodeValue.replace(/ing/g, "img")
+            node.nodeValue = node.nodeValue.replace(/ls/g, "lms")
+            node.nodeValue = node.nodeValue.replace(/lg/g, "lmg")
+            node.nodeValue = node.nodeValue.replace(/ns/g, "ms")
+            node.nodeValue = node.nodeValue.replace(/nts/g, "mts")
+            node.nodeValue = node.nodeValue.replace(/on/g, "om")
+            node.nodeValue = node.nodeValue.replace(/og/g, "omg")
+            node.nodeValue = node.nodeValue.replace(/rce/g, "rmce")
+            node.nodeValue = node.nodeValue.replace(/rd/g, "rmd")
+            node.nodeValue = node.nodeValue.replace(/rt/g, "rmt")
+            node.nodeValue = node.nodeValue.replace(/rk/g, "rmk")
+            node.nodeValue = node.nodeValue.replace(/ys/g, "yms")
+        }
     }
 }())
