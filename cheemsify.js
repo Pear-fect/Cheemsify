ce624abf5907a409de54c27e01cb5a38fea76df1
Pear-fect <ebbe.arildsson@gmail.com>
2020-10-28 13:07:13 +0100

    refactor

--- cheemsify.js
+++ cheemsify.js
@@ -1,23 +1,28 @@
 javascript:(function(){ 
     var html = document.querySelector('html')
     var walker = document.createTreeWalker(html, NodeFilter.SHOW_TEXT)
+    var search = {
+        "am": /an/g,
+        "ems": /es/g,
+        "emp": /ep/g,
+        "img": /ing/g,
+        "lms": /ls/g,
+        "lmg": /lg/g,
+        "ms": /ns/g,
+        "mts": /nts/g,
+        "om": /on/g,
+        "omg": /og/g,
+        "rmce": /rce/g,
+        "rmd": /rd/g,
+        "rmt": /rt/g,
+        "yms": /rk/g,
+    }
     var node
     while (node = walker.nextNode()) {
         if(node.nodeValue.length > 3 && Math.random() > 0){
-            node.nodeValue = node.nodeValue.replace(/an/g, "am")
-            node.nodeValue = node.nodeValue.replace(/es/g, "ems")
-            node.nodeValue = node.nodeValue.replace(/ing/g, "img")
-            node.nodeValue = node.nodeValue.replace(/ls/g, "lms")
-            node.nodeValue = node.nodeValue.replace(/lg/g, "lmg")
-            node.nodeValue = node.nodeValue.replace(/ns/g, "ms")
-            node.nodeValue = node.nodeValue.replace(/nts/g, "mts")
-            node.nodeValue = node.nodeValue.replace(/on/g, "om")
-            node.nodeValue = node.nodeValue.replace(/og/g, "omg")
-            node.nodeValue = node.nodeValue.replace(/rce/g, "rmce")
-            node.nodeValue = node.nodeValue.replace(/rd/g, "rmd")
-            node.nodeValue = node.nodeValue.replace(/rt/g, "rmt")
-            node.nodeValue = node.nodeValue.replace(/rk/g, "rmk")
-            node.nodeValue = node.nodeValue.replace(/ys/g, "yms")
+            for([key, val] of Object.entries(search)) {
+                node.nodeValue = node.nodeValue.replace(val, key)
+            }
         }
     }
 }())
